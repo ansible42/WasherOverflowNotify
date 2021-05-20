9fb7e239e71cb605a2679497b74528e11174ea5b
Taylor Kendall <taylorkendall@gmail.com>
2021-05-20 04:44:12 +0000

    Adding emailer

--- main.go
+++ main.go
@@ -5,15 +5,25 @@ import (
 	"github.com/stianeikeland/go-rpio"
 	"os"
 	"time"
+	"net/smtp"
 )
 
 var (
 	// Use mcu pin 22, corresponds to GPIO 3 on the pi
 	pin = rpio.Pin(22)
 )
+// smtpServer data to smtp server
+type smtpServer struct {
+	host string
+	port string
+   }// Address URI to smtp server
+   func (s *smtpServer) Address() string {
+	return s.host + ":" + s.port
+   }
 
 func main() {
 	// Open and map memory to access gpio, check for errors
+	send("The water monitoring system is now up and running.")
 	if err := rpio.Open(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -23,7 +33,7 @@ func main() {
 
 	pin.Input()
 	pin.PullUp()
-
+	send()
 	for {
 		if pin.Read() == rpio.High {
 			fmt.Println("Sensor has been activated, take action!!")
@@ -31,4 +41,23 @@ func main() {
 		}
 		time.Sleep(1 * time.Second)
 	}
+}
+//Sends a message to us 
+func send(msg string) {    
+	s// Sender data.
+    from := "42depressedrobot@gmail.com"
+    password := "NSQPnCr7oz%79%b"    
+	// Receiver email address.
+    to := []string{
+        "taylorkendall@gmail.com",
+		"JenniferAntos@gmai.com"
+    }    
+	// smtp server configuration.
+    smtpServer := smtpServer{host: "smtp.gmail.com", port: "587"}    
+    message := []byte(msg)  
+    auth := smtp.PlainAuth("", from, password, smtpServer.host)    
+    err := smtp.SendMail(smtpServer.Address(), auth, from, to, message)    if err != nil {
+        fmt.Println(err)
+        return
+    }    fmt.Println("Email Sent!")
 }
